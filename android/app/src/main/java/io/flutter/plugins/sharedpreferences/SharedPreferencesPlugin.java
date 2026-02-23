ef5d03b1ae284237285d26d3d51d6f1021822dbc
Ravi Kumar E <ravikumarece2021@mvit.edu.in>
2026-02-23 19:38:30 +0530

    ci: fix android duplicate plugin and macOS release zip path

--- android/app/src/main/java/io/flutter/plugins/sharedpreferences/SharedPreferencesPlugin.java
+++
@@ -1,6 +0,0 @@
-package io.flutter.plugins.sharedpreferences;
-
-/**
- * Compatibility shim for projects with a checked-in GeneratedPluginRegistrant.
- */
-public class SharedPreferencesPlugin extends LegacySharedPreferencesPlugin {}
